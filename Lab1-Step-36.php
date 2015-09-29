42aee2d10bba4305196695150232d1fe9416d0d1
RavenCastaneda <raven.castaneda@uoit.net>
2015-09-28 23:54:16 -0400

    Create Lab1-Step-36.php

---
+++ Lab1-Step-36.php
@@ -0,0 +1,13 @@
+<?php
+$a1=$_POST['a'];
+$b1=$_POST['b'];
+$c1=$_POST['c'];
+$d1=$_POST['d'];
+$e1=$_POST['e'];
+
+echo "<p>Greetings $a1 $b1</p>";
+echo "<p>Your username is: $c1 and you password is: $d1</p>";
+echo "<p>Your cellphone number is $e1</p>";
+
+echo "<p><hr color='darkblue' size='4'>End of Part3</p>";
+?>
